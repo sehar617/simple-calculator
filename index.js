3f3342a83f988e1ebf63336ee23a338a5c4b7a39
sehar617 <seharrajpoot549@gmail.com>
2024-07-25 03:58:12 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+num1 = parseFloat(prompt("Enter the first number"));
+num2 = parseFloat(prompt("Enter the second number"));
+
+ let sum = num1 + num2;
+let subtraction = num1 - num2;
+ let product = num1 * num2;
+ let modulus = num1 % num2;
+
+
+
+document.write("Sum: " + sum   + "<br>");
+document.write("subtraction: " + subtraction + "<br>"  );
+document.write("Product: " + product + "<br>" );
+document.write("Modulus: " + modulus + "<br>");
+
+
+alert("Sum: " + sum + "\nsubtraction: " + subtraction+ "\nproduct: " + product + "\nModulus: " + modulus );
+
+console.log("Sum: " + sum);
+console.log("subtraction: " + subtraction);
+console.log("Product: " + product);
+console.log("Modulus: " + modulus);
